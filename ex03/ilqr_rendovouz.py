84c1c592aac1a663cd57ca526ca4b9336b98330c
Lukas Leindals <46963040+s183920@users.noreply.github.com>
2020-03-08 20:05:36 +0100

    rm Ex 03

--- ex03/ilqr_rendovouz.py
+++
@@ -1,8 +0,0 @@
-"""
-This file may not be shared/redistributed freely. Please read copyright notice in the git repo.
-"""
-from irlc.ex03.ilqr_rendovouz_basic import solve_rendovouz
-
-if __name__ == "__main__":
-    solve_rendovouz(use_linesearch=True)
-
