e95b22daa9aff9586aea256cbaaae8ea73a01ab8
Peter Grønning <s183922@win.dtu.dk>
2020-03-09 13:36:46 +0100

    Importet model of AutoVc

--- Model/VAE.py
+++ Model/VAE.py
@@ -1,1 +1,9 @@
 import torch
+import os
+import sys
+path = sys.path[0]
+os.chdir(path)
+use_cuda = torch.cuda.is_available()
+device = torch.device("cuda" if use_cuda else "cpu")
+model = torch.load("WaveNetVC_pretrained.pth", map_location = torch.device(device))
+
