51303e64ce3c7f2ba3c08df7e2551a325f7c1600
Peter Grønning <s183922@win.dtu.dk>
2020-03-23 18:55:17 +0100

    Update Preprocessing_WAV.py
    Made it nice with classes:
    TODO: Silence Trimming

--- Kode/Preprocessing_WAV.py
+++ Kode/Preprocessing_WAV.py
@@ -5,61 +5,59 @@ import librosa.display
 import matplotlib.pyplot as plt
 import numpy as np
 
-def spec_Mel(path):
-    #Returns the spectogram matrix of a desired filename
-    filename = path
-    y, sr = librosa.load(filename)
-    # trim silent edges
-    Test, _ = librosa.effects.trim(y, top_db = 1000)
+class Preproccesing:
+    def __init__(self, sr = 16000, n_fft = 1024, hop = 256, n_mels = 80, ref_db = 20):
+        self.sampling_rate = sr
+        self.n_fft = n_fft
+        self.hop_length = hop
+        self.n_mels = n_mels
+        self.ref_db = ref_db
+        self.rescaling_max = 0.999
 
-    #librosa.display.waveplot(Test, sr=sr);
+    def spec_Mel(self, path):
+        """
+        Creates Mel Spectrogram from Waveform (.wav).
+        Input: .wav filepath
+        Output: 80 dim normalised mel-spectrogram
+        """
+        filename = path
 
-    print("Sampling Rate: {}".format(sr))
-    n_fft = 1024 #frame length. The (positive integer) number of samples in an analysis window (or frame). This is denoted by an integer variable n_fft.
-    hop_length = 256  #The number of samples between successive frames, e.g., the columns of a spectrogram. This is denoted as a positive integer hop_length.
-    n_mels = 80 #the non linear transformation value to transform HZ to mel's
-    sample_rate = sr
-    fmin = 40
-    fmax = 7900
-    mel = librosa.filters.mel(sr=sample_rate, n_fft=n_fft, n_mels=n_mels)
+        # Load and rescale signal
+        y, sr = librosa.load(filename, sr=self.sampling_rate)
+        rescaled_signal = y / np.abs(y).max() * self.rescaling_max
+
+        # Short Time Fourier Transformation
+        STFT = librosa.core.stft(rescaled_signal, n_fft=self.n_fft, hop_length=self.hop_length)
+
+        # Convert to power spectrum, and from power to DB using reference power = ref_db
+        power = abs(STFT)**2
+        S_DB = librosa.power_to_db(power, ref = self.ref_db)
+
+        # Compute Mel-Filterbank with n_mels (default = 80) mels
+        mel_basis = librosa.filters.mel(self.sampling_rate, self.n_fft, self.n_mels)
+
+        # Create Mel Spectrogram from STFT and Mel filterbank - Normalise: [0 , 1]
+        mel_spectrogram = np.dot(mel_basis, S_DB)
+        norm_mel = (mel_spectrogram - np.min(mel_spectrogram)) / (np.max(mel_spectrogram) - np.min(mel_spectrogram))
+        self.Mel_spectrogram = norm_mel
+
+        return self.Mel_spectrogram
+
+    def ShowSpec(self, librosa = False):
+        """
+        Shows mel spectrogram
+        if librosa - plots librosas mel option.
+        """
+        if librosa:
+            librosa.display.specshow(self.Mel_spectrogram, sr = self.sampling_rate, hop_length = self.hop_length,
+                                 y_axis = 'fft', x_axis = 's')
+        else:
+            plt.matshow(self.Mel_spectrogram)
+            plt.colorbar()
+        plt.show()
+
+    def TrimSilence(self):
+        TODO
 
 
-    S = librosa.feature.melspectrogram(Test, sr=sample_rate, n_fft=n_fft,
-                                       hop_length=hop_length,
-                                       n_mels=n_mels, fmin = fmin, fmax = fmax)
-    S_DB = librosa.power_to_db(S, ref=np.max) #Shape: 128 X 203
 
-    print('Shape of the matrix: {}'.format(np.shape(S)))
-    return S, S_DB
-
-
-
-def plot_Mel(path):
-    #creates a Mel spectogram plot of the desired filename, with path being a string.
-    filename = path
-    y, sr = librosa.load(filename)
-    # trim silent edges
-    Test, _ = librosa.effects.trim(y)
-    librosa.display.waveplot(Test, sr=sr);
-
-    n_fft = 1024  # frame length. The (positive integer) number of samples in an analysis window (or frame). This is denoted by an integer variable n_fft.
-    hop_length = 256  # The number of samples between successive frames, e.g., the columns of a spectrogram. This is denoted as a positive integer hop_length.
-    n_mels = 80  # the non linear transformation value to transform HZ to mel's
-    fmin = 125
-    fmax = 7600
-    mel = librosa.filters.mel(sr=sr, n_fft=n_fft, n_mels=n_mels)
-
-    S = librosa.feature.melspectrogram(Test, sr=sr, n_fft=n_fft,
-                                       hop_length=hop_length,
-                                       n_mels=n_mels, fmin = fmin, fmax
-                                        = fmax)
-    S_DB = librosa.power_to_db(S, ref=np.max)  # Shape: 128 X 203
-    librosa.display.specshow(S_DB, sr=sr, hop_length=hop_length,
-                             x_axis='time', y_axis='mel');
-    plt.colorbar(format='%+2.0f dB');
-
-def play_Spec(spectrogram):
-    #spectrogram, _ = spectrogram
-    Sound = librosa.feature.inverse.mel_to_stft(spectrogram)
-    y = librosa.griffinlim(Sound)
-    return plt.plot(y)
