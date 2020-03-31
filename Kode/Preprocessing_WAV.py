b72879629bd3bad3ffd881e766d4a920133dfe91
Peter Grønning <s183922@win.dtu.dk>
2020-03-31 12:10:54 +0200

    Speaker Identity encoder update
    Transferred scripts from 'Real_time_Voice_cloning' - repo to work with ours.
    
    Speaker Identy encoder - should work npw

--- Kode/Preprocessing_WAV.py
+++ Kode/Preprocessing_WAV.py
@@ -45,18 +45,19 @@ class Preproccesing:
 
                 # Convert to power spectrum, and from power to DB using reference power = ref_db
                 power = abs(STFT)**2
-                S_DB = librosa.power_to_db(power, ref = self.ref_db)
+                #S_DB = librosa.power_to_db(power, ref = self.ref_db)
 
                 # Compute Mel-Filterbank with n_mels (default = 80) mels
                 mel_basis = librosa.filters.mel(self.sampling_rate, self.n_fft, self.n_mels)
 
                 # Create Mel Spectrogram from STFT and Mel filterbank - Normalise: [0 , 1]
                 mel_spectrogram = np.dot(mel_basis, S_DB)
-                norm_mel = (mel_spectrogram - np.min(mel_spectrogram)) / (np.max(mel_spectrogram) - np.min(mel_spectrogram))
-                self.Mel_spectrogram = np.log(norm_mel.T)
+                #norm_mel = (mel_spectrogram - np.min(mel_spectrogram)) / (np.max(mel_spectrogram) - np.min(mel_spectrogram))
+                self.Mel_spectrogram = np.log(mel_spectrogram.T)
                 self.mel_specs[i].append(self.Mel_spectrogram)
                 print(np.shape(self.Mel_spectrogram))
         return self.mel_specs
+
 
     def ShowSpec(self, librosa = False):
         """
@@ -74,70 +75,5 @@ class Preproccesing:
                     plt.colorbar()
             plt.title(self.labels[i])
             plt.show()
-
-    def slicer(self, slice_size, mel_list = None, n_mels=None):
-        """
-        Takes a batch of mel_spectrograms (represented as a list of list),
-        slice size and number of mels.
-        Computes 50 % overlapping slices of even length (slice_size) of spectrograms and
-        concatenates every slice into one 3 d matrix of dimension: a x n_mels x slice_size
-        where 'a' depends on the batch size and how the spectrograms are sliced.
-        If a spectrogram is not shaped correctly for slicing, zeropadding is performed.
-
-        Inpus Params:
-        mel_list: list of list contaning mel_spectrograms
-            mel_list[i] = list of spectrograms belonging to person i
-            mel_list[i][j] = spectrogram j belonging to person i
-        slice_size: (int) the size of each slice (works only for even numbers)
-        n_mels: (int) the number of mel channels of the spectrograms
-
-        Output:
-        stack_of_slices: 3d matrix of spectrogram slices
-        list_of_slices: list of n slices, where n is the number of melspectrograms.
-                        slice i indicate where to slice stack_of_slices to get slices
-                        corresponding to spectrogram i.
-
-        """
-        # Initialize outputs
-        if mel_list == None:
-            mel_list = self.mel_specs
-        if n_mels == None:
-            n_mels = self.n_mels
-
-        stack_of_slices = np.empty((1, slice_size,n_mels))
-        list_of_slices = []
-        old = 0
-
-        # Enumerate over batch
-        for i, label in enumerate(mel_list):
-            for j, A in enumerate(label):
-
-                xA, yA = np.shape(A)  # Gets shape of spectrogram
-
-                # Perform zero-padding
-                diff = xA % int(slice_size / 2)
-                to_pad = int((slice_size / 2 - diff) % (slice_size / 2))
-                padding = np.zeros((to_pad,yA))
-                A_pad = np.vstack((A, padding))
-
-                # Calculate number of slices
-                num_slices = int((xA + to_pad) / (slice_size / 2)) - 1
-                list_of_slices.append(slice(old, old + num_slices))
-                old += num_slices
-
-                # Slice spectrogram and append to mels
-
-                mels = []
-                for k in range(int(num_slices)):
-                    S = slice(int(k * slice_size / 2), int(k * slice_size / 2 + slice_size))
-                    A_slice = A_pad[S]
-                    mels.append(A_slice)
-
-                # Stacks slices to matrix and stacks with the rest of sliced spectrograms
-                mels = np.stack(mels, axis=0)
-                stack_of_slices = np.vstack((stack_of_slices, mels))
-            print("slice: ", np.shape(stack_of_slices))
-        return stack_of_slices[1:], list_of_slices
-
 
 
--- Model/VAE.py
+++ Model/VAE.py
@@ -2,106 +2,44 @@ import os, sys
 os.chdir(".")
 sys.path.append(os.path.abspath(os.curdir))
 import torch
-from Model.AutoVC.autovc_master.synthesis import build_model
-from Model.AutoVC.autovc_master.synthesis import wavegen
-from Kode.Preprocessing_WAV import Preproccesing
-import Kode.Data as Data
-import pickle
-import librosa
 import numpy as np
 import matplotlib.pyplot as plt
-from Model.SpeakerEncoder import SpeakerEncoder
-from ge2e import GE2ELoss
 from Kode.dataload import DataLoad
-from scipy.linalg import svd
 import seaborn as sns
-from sklearn.neighbors import KNeighborsClassifier as  KNN
-from sklearn.metrics import accuracy_score
-from sklearn.metrics.pairwise import cosine_similarity as cos
-import umap
 from sklearn.manifold import TSNE
-#import umap.plot
+from Model.encoder.inference import load_model as load_encoder
+from Model.encoder.audio import preprocess_wav
+import librosa
+from Model.encoder.inference import embed_utterance
 path = sys.path[0]
 os.chdir(path)
 
 # Check for GPU
 use_cuda = torch.cuda.is_available()
 device = torch.device("cuda" if use_cuda else "cpu")
-"""
-try:
-    checkpoint = torch.load("WaveNetVC_pretrained.pth", map_location = torch.device(device))
-except FileNotFoundError:
-    checkpoint = torch.load("Y:/Desktop/fagprojekt/WaveNetVC_pretrained.pth", map_location = torch.device(device))
-"""
-#WaveNet = build_model().to(device)
-#WaveNet.load_state_dict(checkpoint["state_dict"])
 
 Data = DataLoad("../Kode/Data")
+encoder = load_encoder("../Real_Time_Voice_Cloning/encoder/pretrained.pt").float()
 
-from Real_Time_Voice_Cloning.encoder.inference import load_model
+def SpeakerIdentity(Data):
+    embedding = []
+    labels = []
+    for j in Data:
+        for path in Data[j]:
+            print(path)
+            labels.append(j)
+            y = librosa.load(path, sr = 16000)[0]
+            y = preprocess_wav(y)
+            embed = embed_utterance(y)
+            embedding.append(embed)
+    return np.array(embedding), labels
 
-model = load_model("../Real_Time_Voice_Cloning/encoder/pretrained.pt").float()
-
-
-
-def speaker_identity(Data, model, window_widt, step, sample_rate):
-    n_fft = int(sample_rate * window_widt / 1000)
-    hop_size = int(sample_rate * step / 1000)
-    Prep = Preproccesing(sr = sample_rate, n_fft = n_fft, hop = hop_size)
-    Prep.spec_Mel(Data.head(n=9).to_numpy().T.tolist(), Data.columns.tolist())
-
-    Slices, b = Prep.slicer(160)
-    Slices = torch.from_numpy(Slices).float()
-    embedding = model.forward(torch.clone(Slices).detach())
-    embedding = embedding.detach().numpy()
-    speaker_id = np.array([np.mean(embedding[B], axis = 0) for B in b])
-    return speaker_id, Prep.labels
-embedding, labels = speaker_identity(Data, model, 25, 10, 16000)
-
-
-print(np.shape(embedding))
-U, s, vh = svd(embedding)
-print(np.shape(vh), np.shape(s))
-PC = vh
-print(np.shape(PC), np.shape(embedding))
-Z = embedding @ PC.T
-
-print(np.shape(Z))
+def EvalEmbedding(embedding, labels):
+    X = TSNE(n_components=2 ).fit_transform(embedding)
+    sns.scatterplot(X[:, 0], X[:, 1], hue=labels)
+    plt.title("t-SNE")
+    plt.show()
 
 
-label = labels
-cols = ["green", "red", "orange","blue", "magenta", "skyblue", "navy", "darkgreen", "black", "orangered"]
-di = {p: cols[i] for i,p in enumerate(label)}
-
-plt.plot(np.cumsum(s**2)/np.sum(s**2))
-plt.show()
-
-
-mu = np.array([np.mean(embedding[slice(i*9, i*9 + 9)], axis = 0) for i in range(10)])
-KNN = KNN(n_neighbors=1)
-KNN = KNN.fit(mu, np.arange(10))
-ye = KNN.predict(embedding)
-print("KNN accuracy:" ,accuracy_score(np.sort([0,1,2,3,4,5,6,7,8,10]*9), ye))
-
-
-sns.scatterplot(Z[:,0], Z[:,1], hue = sorted(label*9), palette=di)
-plt.title("PCA")
-plt.show()
-
-
-m = umap.UMAP().fit(embedding)
-sns.scatterplot(m.embedding_[:,0], m.embedding_[:,1], hue = sorted(label*9), palette=di)
-
-plt.title("UMAP")
-plt.show()
-
-
-X = TSNE(n_components=2).fit_transform(embedding)
-sns.scatterplot(X[:,0], X[:,1], hue = sorted(label*9), palette=di)
-plt.title("t-SNE")
-plt.show()
-
-
-
-
-
+embedding, labels = SpeakerIdentity(Data.head(n = 9))
+EvalEmbedding(embedding, labels)
