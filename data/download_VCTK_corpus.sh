60665c49aeeb73de79b86d2bb0717f68f03f271a
Lukas Leindals <s183920@student.dtu.dk>
2020-04-13 18:46:59 +0200

    update download_VCTK_corpus.sh

--- data/download_VCTK_corpus.sh
+++ data/download_VCTK_corpus.sh
@@ -1,12 +1,23 @@
-if [ -d "data/VCTK-Data" ] 
+#!/bin/bash
+
+
+
+
+if [ $PWD = ~/*/Deep_voice_conversion/data ]
 then
-	echo "Directory exists"
-else
-	mkdir data/VCTK-Data
-	wget https://datashare.is.ed.ac.uk/bitstream/handle/10283/2651/VCTK-Corpus.zip?sequence=2&isAllowed=y
-	unzip VCTK-Corpus.zip -d ../data/VCTK-Data
+	if [ -d "VCTK-Data" ] 
+	then
+		echo -e "\e[32mVCTK corpus already downloaded!\e[0m"
+	else
+		mkdir VCTK-Data
+		wget https://datashare.is.ed.ac.uk/bitstream/handle/10283/2651/VCTK-Corpus.zip?sequence=2&isAllowed=y
+		unzip VCTK-Corpus.zip -d VCTK-Data
+		
+		###If the downloaded VCTK is in tar.gz, run this:
+		#tar -xzvf VCTK-Corpus.tar.gz -C ../data/VCTK-Data
+	fi
 	
-	###If the downloaded VCTK is in tar.gz, run this:
-	#tar -xzvf VCTK-Corpus.tar.gz -C ../data/VCTK-Data
-    
+else
+	echo -e "\e[31mHave you made sure to run this scripts from the 'Deep_voice_conversion/data' directory?\e[0m"
+
 fi
