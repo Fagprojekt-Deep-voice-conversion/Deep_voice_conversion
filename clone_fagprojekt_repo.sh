ab6d0feb28d51e450a6b726a0a38ce9dc09844a4
Lukas Leindals <s183920@student.dtu.dk>
2020-04-15 13:26:34 +0200

    add clone

---
+++ clone_fagprojekt_repo.sh
@@ -0,0 +1,9 @@
+### Download git repo
+if [ -d "../Deep_voice_conversion" ]
+then 
+	echo "Repo already downloaded"
+else
+	git clone https://github.com/Fagprojekt-Deep-voice-conversion/Deep_voice_conversion.git
+
+fi
+
