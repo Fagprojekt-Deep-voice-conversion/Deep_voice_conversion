358f95619fab03211fbd1a7e4789586a899043f1
Peter <s183922@student.dtu.dk>
2020-04-16 09:38:06 +0200

    Thinlinch changes

--- Model/Run_AutoVC.py
+++ Model/Run_AutoVC.py
@@ -17,17 +17,17 @@ data, labels = DataLoad2(data_path)
 
 print("Number of wav files: {:}".format(len(data)))
 batch_size = 2
-num_workers = 0
+num_workers = 8
 shuffle = True
 
 
-n_steps = 2
+n_steps = 100000
 save_every = 10000
-models_dir = "Models"
+models_dir = "Training"
 model_path_name = "trainedModel"
 loss_path_name = "loss"
 
-trainloader = TrainLoader(data[:5], labels[:5], batch_size = batch_size, shuffle = shuffle, num_workers = num_workers)
+trainloader = TrainLoader(data[-20:], labels[-20:], batch_size = batch_size, shuffle = shuffle, num_workers = num_workers)
 Train(trainloader, n_steps, save_every, models_dir, model_path_name, loss_path_name)
 
 
