e746ae018fbbf18a44b9bfede3c5ce43ac71aa64
Lukas Leindals <s183920@student.dtu.dk>
2020-04-29 17:04:22 +0200

    make setup file for star gan a '

---
+++ StarGAN/run_scripts/setup.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+### Set directory
+
+if [ "$1" = "" ]
+then 
+	echo -e "\e[31mMissing a directory to do the setup in\e[0m"
+	exit 1
+else
+	echo "$1"	
+	cd $1
+fi
+
+if [ ! -d "$1" ] 
+then
+	echo -e "\e[31mSetup directory does not exist!\e[0m"
+	exit 1
+fi
+
+
+
+### Make python environment
+module load python3
+python3 -m venv StarGAN-env
+
+source StarGAN-env/bin/activate
+
+python -m pip install --upgrade cython
+python -m pip install torch==0.4.0 pyworld tqdm librosa tensorboardX tensorboard
+
+deactivate
