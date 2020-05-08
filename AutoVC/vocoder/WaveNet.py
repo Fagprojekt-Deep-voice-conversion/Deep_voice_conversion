e2e1261df420710bb5f999ab398530f8fecf78dc
Peter Grønning <s183922@win.dtu.dk>
2020-05-08 11:22:49 +0200

    Vocoder and conversion

--- AutoVC/vocoder/WaveNet.py
+++ AutoVC/vocoder/WaveNet.py
@@ -1,6 +1,11 @@
+import os,sys,inspect
+current_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parent_dir = os.path.dirname(current_dir)
+sys.path.insert(0, parent_dir) 
+
 import torch
 from tqdm import tqdm
-from AutoVC.hparams import hparams_autoVC as hparams
+from hparams import hparams_autoVC as hparams
 from wavenet_vocoder import builder
 
 torch.set_num_threads(4)
--- AutoVC/vocoder/WaveRNN_inference.py
+++ AutoVC/vocoder/WaveRNN_inference.py
@@ -1,11 +1,17 @@
+import os,sys,inspect
+current_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parent_dir = os.path.dirname(current_dir)
+sys.path.insert(0, parent_dir) 
 import torch
-from WaveRNN_model import WaveRNN
-from AutoVC.hparams import hparams_waveRNN as hp
+print(sys.path[0])
+from vocoder.WaveRNN_model import WaveRNN
+
+from hparams import hparams_waveRNN as hp
 import librosa
 import numpy as np
 import matplotlib.pyplot as plt
-import os,sys; os.chdir(sys.path[0])
-from AutoVC.Preprocessing_WAV import WaveRNN_Mel
+
+from Preprocessing_WAV import WaveRNN_Mel
 
 
 def Generate(m):
@@ -32,7 +38,7 @@ def Generate(m):
                         sample_rate=hp.sample_rate,
                         mode='MOL').to(device)
 
-    voc_model.load('utils/WaveRNN_Pretrained.pyt')
+    voc_model.load('Models/WaveRNN/WaveRNN_Pretrained.pyt')
 
 
 
@@ -47,7 +53,3 @@ def Generate(m):
 
     librosa.output.write_wav("test1" + '.wav', np.asarray(waveform), sr = hp.sample_rate)
 
-M = WaveRNN_Mel("p225_001.wav")
-
-
-Generate(M)
