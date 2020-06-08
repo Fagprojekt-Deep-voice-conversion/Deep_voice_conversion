2c085859145bed1d934c3f985399a2a455adaa26
Peter Grønning <s183922@win.dtu.dk>
2020-06-08 09:31:21 +0200

    Conversion
    Made conversion script for experiments

--- AutoVC/conversion.py
+++ AutoVC/conversion.py
@@ -9,76 +9,112 @@ from vocoder.WaveNet import wavegen
 from Speaker_encoder.audio import preprocess_wav
 from Speaker_encoder.inference import load_model as load_encoder
 from Speaker_encoder.inference import embed_utterance
-from vocoder.WaveRNN_inference import Generate
 from vocoder.WaveRNN_model import WaveRNN
 from hparams import hparams_waveRNN as hp
 import pickle
+import os
 
-def Conversion(source, target, model,  vocoder = "wavenet", sound_out = False, Visualize = False):
+def Instantiate_Models(model_path,  vocoder = "wavernn", sound_out = False, Visualize = False):
+    use_cuda = torch.cuda.is_available()
+    device = torch.device("cuda" if use_cuda else "cpu")
 
+    # Prepare vocoder
     if vocoder == "wavenet":
-        s, t = AutoVC_Mel(source), AutoVC_Mel(target)
+        
+        # Instantiate WaveNet Model
+        voc_model = build_model().to(device)
+        checkpoint = torch.load("Models/WaveNet/WaveNetVC_pretrained.pth", map_location=torch.device(device))
+        voc_model.load_state_dict(checkpoint["state_dict"])
+
     elif vocoder == "wavernn": 
-        s, t = WaveRNN_Mel(source), WaveRNN_Mel(target)
+        
+        # Instantiate WaveRNN Model
+        voc_model = WaveRNN(rnn_dims=hp.voc_rnn_dims,
+                            fc_dims=hp.voc_fc_dims,
+                            bits=hp.bits,
+                            pad=hp.voc_pad,
+                            upsample_factors=hp.voc_upsample_factors,
+                            feat_dims=hp.num_mels,
+                            compute_dims=hp.voc_compute_dims,
+                            res_out_dims=hp.voc_res_out_dims,
+                            res_blocks=hp.voc_res_blocks,
+                            hop_length=hp.hop_length,
+                            sample_rate=hp.sample_rate,
+                            mode='MOL').to("cpu")
+
+        voc_model.load('Models/WaveRNN/WaveRNN_Pretrained.pyt')
     else:
         raise RuntimeError("No vocoder chosen")
 
-    S, T = torch.from_numpy(s.T).unsqueeze(0), torch.from_numpy(t.T).unsqueeze(0)
-    
-    S_emb, T_emb = embed(source), embed(target)
-    
-    conversions = {"SS": (S, S_emb, S_emb), "ST": (S, S_emb, T_emb), "TT": (T, T_emb, T_emb), "TS": (T, T_emb, S_emb)}
-    converted_numpy = []
-    converted_tensor = []
-    for key, (X, c_org, c_trg) in conversions.items():
-        _, Out, _ = model(X, c_org, c_trg)
-        Out1 = Out.squeeze(0).squeeze(0).detach().numpy()
-        converted_numpy.append(Out1)
-        converted_tensor.append(Out)
+    # Prepare AutoVC model
+    model = Generator(32, 256, 512, 32).eval().to(device)
+    g_checkpoint = torch.load("Models/AutoVC/" + model_path + ".pt", map_location=torch.device(device))
+    model.load_state_dict(g_checkpoint['model_state'])
+
+    # Prepare Speaker Encoder Module
+    load_encoder("Models/SpeakerEncoder/SpeakerEncoder.pt").float()
         
-    return S.squeeze(0).detach().numpy(), T.squeeze(0).detach().numpy(), tuple(converted_numpy)
+    return model, voc_model
 
 def embed(path):
     y = librosa.load(path, sr = 16000)[0]
     y = preprocess_wav(y)
     return torch.tensor(embed_utterance(y)).unsqueeze(0)
+
+def Generate(m, fpath, model, modeltype = "wavernn"):
     
-load_encoder("Models/SpeakerEncoder/SpeakerEncoder.pt").float()
+    if modeltype == "wavernn":
+        m = m.squeeze(0).squeeze(0).T.unsqueeze(0)
+        waveform = model.generate(m, batched = True, target = 11_000, overlap = 550, mu_law= False)
 
-model = Generator(32, 256, 512, 32).eval().to("cpu")
-g_checkpoint = torch.load("Models/AutoVC/autoVC_seed40_original_200k.pt", map_location=torch.device("cpu"))
-model.load_state_dict(g_checkpoint['model_state'])
-data, labels = DataLoad2("Test_Data")
+    elif modeltype == "wavenet":
+        m = m.squeeze(0).squeeze(0)
+        waveform = wavegen(model, m)
 
-
-# Instantiate WaveRNN Model
-voc_model = WaveRNN(rnn_dims=hp.voc_rnn_dims,
-                    fc_dims=hp.voc_fc_dims,
-                    bits=hp.bits,
-                    pad=hp.voc_pad,
-                    upsample_factors=hp.voc_upsample_factors,
-                    feat_dims=hp.num_mels,
-                    compute_dims=hp.voc_compute_dims,
-                    res_out_dims=hp.voc_res_out_dims,
-                    res_blocks=hp.voc_res_blocks,
-                    hop_length=hp.hop_length,
-                    sample_rate=hp.sample_rate,
-                    mode='MOL').to("cpu")
-
-voc_model.load('Models/WaveRNN/WaveRNN_Pretrained.pyt')
-
-
-t = data[23]
-s = data[len(data)-25]
-print(labels[len(data)-25], labels[23])
-
-# S, T, (SS, ST, TT, TS)  = Conversion(s, t, model, vocoder = "wavernn", Visualize= False, sound_out= False)
-
-# Generate(ST.T, "Test303", voc_model)
+    librosa.output.write_wav(fpath + '.wav', np.asarray(waveform), sr = hp.sample_rate)
 
 
 
-# X = pickle.load(open("Models/loss_scratch60", "rb"))
-# print(len(X))
-# plt.plot(np.convolve(np.asarray(X[100:]), np.ones(50)/50, "valid"))
-# plt.show()
+
+def Conversion(source, target, model, voc_model, voc_type = "wavernn", task = None, subtask = None):
+    if voc_type == "wavernn":
+        s, t = WaveRNN_Mel(source), WaveRNN_Mel(target)
+    else:
+        s, t = AutoVC_Mel(source), AutoVC_Mel(target)
+
+    S, T = torch.from_numpy(s.T).unsqueeze(0), torch.from_numpy(t.T).unsqueeze(0)
+    
+    S_emb, T_emb = embed(source), embed(target)
+    
+    conversions = {"Source": (S, S_emb, S_emb), "Converted": (S, S_emb, T_emb), "Target": (T, T_emb, T_emb)}
+    
+    dir_size = len(list(os.walk(f"Experiments/{task}/{subtask}"))[0][1]) + 1
+    os.mkdir(f"Experiments/{task}/{subtask}/{dir_size}")
+
+    for key, (X, c_org, c_trg) in conversions.items():
+        if key == "Converted":
+            _, Out, _ = model(X, c_org, c_trg)
+        else:
+            Out = X.unsqueeze(0)
+        
+        
+        path = f"Experiments/{task}/{subtask}/{dir_size}/{key}"
+        print(f"\n Generating {key} sound")
+        Generate(Out, path, voc_model, voc_type)
+
+
+if __name__ == "__main__":
+    data, labels = DataLoad2("Test_Data")
+    t = data[39]
+    s = data[len(data)-25]
+    model, voc_model = Instantiate_Models(model_path = "autoVC_seed80_200k", vocoder = "wavernn", Visualize= False, sound_out= False)
+
+    Conversion(s, t, model, voc_model, task = "English_English", subtask = "Male_Male")
+    
+    # X = pickle.load(open("Models/loss_scratch40", "rb"))
+    # Y = pickle.load(open("Models/loss_scratch60", "rb"))
+    # Z = pickle.load(open("Models/loss_scratch80", "rb"))
+
+    # plt.plot(X); plt.plot(Y); plt.plot(Z)
+    # plt.grid()
+    # plt.show()
--- Experiment/Survey-app/voice-conversion-survey/app.R
+++ Experiment/Survey-app/voice-conversion-survey/app.R
@@ -8,6 +8,11 @@ ui <- fluidPage(
     sidebarPanel(
         h3("Please enter your name"),
         textInput("name", ""),
+        h6(textOutput("save.results")),
+        h6(textOutput("checkcategory")),
+        h6(textOutput("checkcategory1")),
+    
+       
         # h3("First sample"),
         # h4("Sound 1"),
         # tags$audio(src = "3.wav", type = " 'audio/wav", controls = NA),
@@ -30,16 +35,18 @@ ui <- fluidPage(
         actionButton("play3", "Play sound 3"),
         # h4("How well does it sound"),
         sliderInput("item2", "How well does it sound?", 0, 10, value = 5),
-        actionButton("submit", "Submit"),
+        actionButton("submit1", "Submit"),
         textOutput("ty1")),
+       
         #actionButton("scared", "Get haunted!")
         mainPanel(
             # Main Action is where most everything is happenning in the
             # object (where the welcome message, survey, and results appear)
             uiOutput("MainAction"),
             # This displays the action putton Next.
-            actionButton("Click.Counter", "Next"), 
-            actionButton("submit", "Submit")    
+            actionButton("Click.Counter", textOutput("Button")), 
+            
+            textOutput("test")
         )
         
         
@@ -52,22 +59,46 @@ server <- function(input, output){
     #     data.frame(Name = c("Sound 1"),
     #                Value = c(input$item1),
     #                stringsAsFactors = F)})
-
+    
     
     #observeEvent(input$Click.Counter, {output$ty <- renderText({paste0("You gave a score of ",counter)})})
-    observeEvent(input$play1, {insertUI(ui = tags$audio(src = "1.wav", type = " 'audio/wav", autoplay = NA, controls = NA, style="display:none;"), selector = "#play1", where = "afterEnd")})
-    observeEvent(input$play2, {insertUI(ui = tags$audio(src = "2.wav", type = " 'audio/wav", autoplay = NA, controls = NA, style="display:none;"), selector = "#play2", where = "afterEnd")})
-    observeEvent(input$play3, {insertUI(ui = tags$audio(src = "3.wav", type = " 'audio/wav", autoplay = NA, controls = NA, style="display:none;"), selector = "#play3", where = "afterEnd")})
+    observeEvent(input$play11, {insertUI(ui = tags$audio(src = sprintf("%s/%s/%s.wav",
+                                                                       categories[catsamples[input$Click.Counter]],
+                                                                       subcategories[subcatsamples[input$Click.Counter]],
+                                                                       samples[1]), type = " 'audio/wav", autoplay = NA,
+                                                         controls = NA, style="display:none;"), selector = "#play1", where = "afterEnd")})
+    
+    observeEvent(input$play21, {insertUI(ui = tags$audio(src = sprintf("%s/%s/%s.wav", 
+                                                                       categories[catsamples[input$Click.Counter]], 
+                                                                       subcategories[subcatsamples[input$Click.Counter]], 
+                                                                       samples[2]), type = " 'audio/wav", autoplay = NA, 
+                                                         controls = NA, style="display:none;"), selector = "#play2", where = "afterEnd")})
+    
+    observeEvent(input$play31, {insertUI(ui = tags$audio(src = sprintf("%s/%s/%s.wav", 
+                                                                       categories[catsamples[input$Click.Counter]],
+                                                                       subcategories[subcatsamples[input$Click.Counter]],
+                                                                       samples[3]), type = " 'audio/wav", autoplay = NA,
+                                                         controls = NA, style="display:none;"), selector = "#play3", where = "afterEnd")})
     
     observeEvent(input$item1, {output$score1 <- renderText({paste0("You gave a score of ", input$item1)})})
     
     #observeEvent(input$submit, {output$ty <- renderText({paste0("Thanks for answering the survey ", input$name, "!")})})
     #observeEvent(input$scared, {insertUI(ui = img(src = "test.png", width = 350, height = 350), selector = "#scared", where = "afterEnd")})
-    observeEvent(input$submit, {output$ty1 <- renderText({paste0("Thanks for answering the survey ", input$item1, input$item2, input$Click.Counter, "!")})})
+    observeEvent(input$submit1, {output$ty1 <- renderText({paste0("Thanks for answering the survey ", input$item1, input$item2, input$Click.Counter, "!")})})
+    observeEvent(input$submit, {output$test <- renderText({paste0("Your number of clicks: ", input$Click.Counter, results, input$survey, "! omg crazy")})})
     output$MainAction <- renderUI( {
         dynamicUi()
     })
-    results = c(0)
+    
+    sliderresults = rep(0, 5)
+    catresults = rep("", 5)
+    
+    categories = c("Danish_Danish", "English_English", "Danish_English", "English_Danish")
+    subcategories = c("Male_Male", "Female_Female", "Male_Female", "Female_Female")
+    
+    samples <<- sample(3, 3, replace = F)
+    catsamples <<- sample(4,5, replace = T)
+    subcatsamples <<- sample(4,5, replace = T)
     
     dynamicUi <- reactive({
         if (input$Click.Counter==0)
@@ -75,44 +106,99 @@ server <- function(input, output){
                 list(
                     h3("Welcome to Deep Fakes Inc.!"),
                     h6("hej med dig")
+                   
                 )
             )
         
         
         if (input$Click.Counter>0 & input$Click.Counter<=5 ){
-            samples <<- sample(3, 3, replace = F)
             
-            observeEvent(input$play11, {insertUI(ui = tags$audio(src = sprintf("%s.wav", samples[1]), type = " 'audio/wav", autoplay = NA, controls = NA, style="display:none;"), selector = "#play1", where = "afterEnd")})
-            observeEvent(input$play21, {insertUI(ui = tags$audio(src = sprintf("%s.wav", samples[2]), type = " 'audio/wav", autoplay = NA, controls = NA, style="display:none;"), selector = "#play2", where = "afterEnd")})
-            observeEvent(input$play31, {insertUI(ui = tags$audio(src = sprintf("%s.wav", samples[3]), autoplay = NA, controls = NA, style="display:none;"), selector = "#play3", where = "afterEnd")})
+            
+            #observeEvent(eval(parse(text=sprintf("input$play1%s", input$Click.Counter))), {insertUI(ui = tags$audio(src = sprintf("%s.wav", samples[1]), type = " 'audio/wav", autoplay = NA, controls = NA, style="display:none;"), selector = "#play1", where = "afterEnd")})
+            #observeEvent(eval(parse(text=sprintf("input$play2%s", input$Click.Counter))), {insertUI(ui = tags$audio(src = sprintf("%s.wav", samples[2]), type = " 'audio/wav", autoplay = NA, controls = NA, style="display:none;"), selector = "#play1", where = "afterEnd")})
+            #observeEvent(eval(parse(text=sprintf("input$play3%s", input$Click.Counter))), {insertUI(ui = tags$audio(src = sprintf("%s.wav", samples[3]), type = " 'audio/wav", autoplay = NA, controls = NA, style="display:none;"), selector = "#play1", where = "afterEnd")})
+            
+            #observeEvent(input$play21, {insertUI(ui = tags$audio(src = sprintf("%s.wav", samples[2]), type = " 'audio/wav", autoplay = NA, controls = NA, style="display:none;"), selector = "#play2", where = "afterEnd")})
+            #observeEvent(input$play31, {insertUI(ui = tags$audio(src = sprintf("%s.wav", samples[3]), autoplay = NA, controls = NA, style="display:none;"), selector = "#play3", where = "afterEnd")})
             return(
                 list(
                     h3("Welcome to Deep Fakes Inc.!"),
                     h3(input$Click.Counter),
-                    h3("Second sample"),
+                    h3("Sound test number: ", input$Click.Counter),
                     h4(samples[1], samples[2], samples[3]),
                     h6(results[length(results)]),
                     actionButton("play11", "Play sound 1"),
                     actionButton("play21", "Play sound 2"),
                     actionButton("play31", "Play sound 3"),
-                    sliderInput("survey", "How well does it sound?", 0, 10, value = 5)
+                    sliderInput("survey", "How well does it sound?", 0, 10, value = 5),
+                    actionButton(sprintf("Click%s", input$Click.Counter), "Click Me")
                     
                 )
             )}
         if (input$Click.Counter>5)
             return(
                 list(
-                    h4("View aggregate results"),
-                    h4("Thanks for taking the survey!")
-                    
+                    h3("Welcome to Deep Fakes Inc.!"),
+                    h3(input$Click.Counter),
+                    h3("Sound test number: ", input$Click.Counter),
+                    h4(samples[1], samples[2], samples[3]),
+                    h6(results[length(results)]),
+                    actionButton("play11", "Play sound 1"),
+                    actionButton("play21", "Play sound 2"),
+                    actionButton("play31", "Play sound 3"),
+                    h4("Thanks for taking the survey!"),
+                    h5("Your results: ", sliderresults[1], sliderresults[2], 
+                       sliderresults[3], sliderresults[4], sliderresults[5])
                 )
             ) 
             
+    })
+    output$checkcategory <- renderText({
+        if (input$Click.Counter <= 5){
+        return(
+                categories[catsamples[input$Click.Counter]])}
+        else{
+            return()}
+        
+        
             
+    })
+    output$checkcategory1 <- renderText({
+        if (input$Click.Counter <= 5){
+        return(
+            subcategories[subcatsamples[input$Click.Counter]])}
+        else{
+            return()}
+        
+        
         
     })
-    
+    output$Button <- renderText({
+        if (input$Click.Counter >= 5){
+            return("Submit")
+        }
+        else{
+            return("Next")
+        }
+    })
+    output$save.results <- renderText({
+        # After each click, save the results of the radio buttons.
+        if ((input$Click.Counter>0)&(input$Click.Counter<=5)){
+            try(sliderresults[input$Click.Counter] <<- input$survey)
+            return()}
+    })
+    output$save.results1 <- renderText({
+        # After each click, save the results of the radio buttons.
+        if ((input$Click.Counter>0)&(input$Click.Counter<=5)){
+            try(sliderresults[input$Click.Counter] <<- input$survey)
+            return()}
+        
+    })
+    A = 0
+    observeEvent(eval(parse(text=sprintf("input$Click%s", input$Click.Counter))), {A = A + 1}) 
+    output$A <- renderText({return(A)})
 }
 
 
 shinyApp(ui = ui, server = server)
+
