e0335ab78566cc0a51150f6621fc5b110570575f
Peter <s183922@student.dtu.dk>
2020-06-11 14:12:53 +0200

    Thinlinc

--- AutoVC/ConductExperiment.py
+++ AutoVC/ConductExperiment.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
     parser.add_argument('--model', type = str, help = "Which model to use")
-    parser.add_argument('--train_size', type = str, help = "The number of minutes trained. Has to be the name of the experiment file")
+    parser.add_argument('--train_size', default = None, type = str, help = "The number of minutes trained. Has to be the name of the experiment file")
     parser.add_argument('--test_data', type = str, help = "File location of test data folder")
     parser.add_argument('--name_list', type = str, help = "File location of name_list")
     parser.add_argument('--experiment_folder', type = str, help = "File location of experiment folder")
