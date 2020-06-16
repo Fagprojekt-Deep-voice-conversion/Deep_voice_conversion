d8a361127c7999280d69a2587d4ea51f9901e272
Peter Grønning <s183922@win.dtu.dk>
2020-06-16 18:13:13 +0200

    Update Statistics.R

--- Experiment/Survey-app/voice-conversion-survey/Statistics.R
+++ Experiment/Survey-app/voice-conversion-survey/Statistics.R
@@ -11,8 +11,9 @@ ss = "https://docs.google.com/spreadsheets/d/1Y2Hu04dY-chxSPdVgcUefXSTs6zvG6lkzA
 S <- read_sheet(ss, sheet = "Similarity")
 Q <- read_sheet(ss, sheet = "Quality")
 Fool <- read_sheet(ss, sheet = "Fakeness")
-
-
+P <- read_sheet(ss, sheet = "Persons")
+PAV <- read_sheet(ss, sheet = "ConversionsAV")
+PSG <- read_sheet(ss, sheet = "ConversionsSG")
 #### SIMILARITY ####
 data <- S[,-c(1,2,3,4)]
 i <- c(1:28)
@@ -46,4 +47,62 @@ colnames(X) <- c("DMM", "DFF", "DMF", "DFM", "EMM", "EFF", "EMF", "EFM")
 barplot(X, beside = T, legend.text = c("AutoVC", "StarGAN"), col = c("skyblue", "orange"), main = "Fool Score")
 
 
+
+
+#### BASELINE SOUND QUALITY ####
+data <- P[,-c(1,2,3,4)]
+data
+i <- c(1:16)
+data[ , i] <- apply(data[ , i], 2,            # Specify own function within apply
+                    function(x) as.numeric(as.character(x)))
+
+MOS <- colMeans(data, na.rm = T) 
+X <- t(cbind(MOS[c(1:8)], MOS[c(9:16)]))
+
+colnames(X) <- c("Obama", "Trump", "Hillary", "Michelle", "Mette", "Helle", "Lars", "Anders")
+barplot(X, beside = T, legend.text = c("WaveRNN", "WORLD"), col = c("skyblue", "orange"), main = "Baseline Sound Quality / Naturalness")
+
+
+
+#### CONVERSION AUTOVC SOUND QUALITY ####
+data <- PAV[,-c(1,2,3,4)]
+data
+i <- c(1:24)
+data[ , i] <- apply(data[ , i], 2,            # Specify own function within apply
+                    function(x) as.numeric(as.character(x)))
+data
+
+MOS <- colMeans(data, na.rm = T)
+
+
+Y <- matrix(t(MOS), nrow = 3, ncol = 8)
+X
+# X <- t(cbind(MOS[c(1:8)], MOS[c(9:16)]))
+
+colnames(Y) <- c("Obama", "Trump", "Hillary", "Michelle", "Mette", "Helle", "Lars", "Anders")
+barplot(colMeans(Y), beside = T,  col = c("skyblue", "orange"), main = "Conversion AutoVC Sound Quality / Naturalness")
+
+
+
+
+data <- PSG[,-c(1,2,3,4)]
+data
+i <- c(1:24)
+data[ , i] <- apply(data[ , i], 2,            # Specify own function within apply
+                    function(x) as.numeric(as.character(x)))
+data
+
+MOS <- colMeans(data, na.rm = T)
+
+
+X <- matrix(t(MOS), nrow = 3, ncol = 8)
+X
+# X <- t(cbind(MOS[c(1:8)], MOS[c(9:16)]))
+
+colnames(X) <- c("Obama", "Trump", "Hillary", "Michelle", "Mette", "Helle", "Lars", "Anders")
+barplot(colMeans(X), beside = T, col = c("skyblue", "orange"), main = "Conversion AutoVC Sound Quality / Naturalness")
+
+
+
+barplot(rbind(colMeans(Y), colMeans(X)), beside = T, legend.text = c("AutoVC", "StarGAN"), col = c("skyblue", "orange"), main = "Mean Conversion Sound Quality / Naturalness")
 
