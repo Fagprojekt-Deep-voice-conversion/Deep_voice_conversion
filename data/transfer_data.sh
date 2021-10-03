1f9835918dc6563f32b59f389a651970371cb7d4
Lukas Leindals <s183920@student.dtu.dk>
2021-10-03 16:21:23 +0200

    transfer data

---
+++ data/transfer_data.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# zip experiment_data/testspeakers/dansk_lyd.zip -r experiment_data/testspeakers/mette experiment_data/testspeakers/helle
+
+# rsync -r s183920@transfer.gbar.dtu.dk:/work1/s183920/Deep_voice_conversion/data/experiment_data/testspeakers/dansk_lyd.zip . # enter this in folder on linux
+
+
+# pscp -scp s183920@transfer.gbar.dtu.dk:/work1/s183920/Deep_voice_conversion/data/experiment_data/testspeakers/dansk_lyd.zip . # enter this in folder on windows
